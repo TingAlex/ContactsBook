eeec25550d395fd2b92fe8a884957d41f147d14e
Ting <tingalex30@gmail.com>
2018-03-26 11:31:31 +0800

    Fix the bug that the userself tel number isn't shown. (cause i forget to init with a CGReact....)

--- WeekTwoDayThreePointNine/UserselfCell.m
+++ WeekTwoDayThreePointNine/UserselfCell.m
@@ -26,10 +26,12 @@
     if (self) {
         CGRect screen = [[UIScreen mainScreen] bounds];
         self.UserShortPicImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 60, 60)];
-//        self.MessImageView.image=[UIImage imageNamed:@"message.jpg"];
+        //TODO:show user short name rather a picture!
+        self.UserShortPicImageView.image = [UIImage imageNamed:@"message.jpg"];
         self.UserNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 10, 200, 30)];
         self.UserTelLabel.font = [UIFont systemFontOfSize:15];
         self.UserTelLabel.textColor = [UIColor grayColor];
+        self.UserTelLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 40, 200, 30)];
         [self addSubview:self.UserShortPicImageView];
         [self addSubview:self.UserNameLabel];
         [self addSubview:self.UserTelLabel];
